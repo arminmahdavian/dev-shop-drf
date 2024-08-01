d6d9a3757629d9ca58c83f8df6e0142c1da31f62
arminmahdavian <armin.mail007@gmail.com>
2024-08-01 14:01:21 +0100

    initiated products models

--- catalogue/models.py
+++ catalogue/models.py
@@ -20,8 +20,94 @@ class Category(MP_Node):
         verbose_name = "Category"
         verbose_name_plural = "Categories"
 
+    def __str__(self):
+        return self.title
+
+
+class OptionGroup(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+
+    class Meta:
+        verbose_name = "Option Group"
+        verbose_name_plural = "Option Groups"
 
     def __str__(self):
         return self.title
 
+
+class OptionGroupValue(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    group = models.ForeignKey(OptionGroup, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Option Group Value"
+        verbose_name_plural = "Option Group Values"
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    description = models.CharField(max_length=2048, null=True, blank=True)
+    slug = models.SlugField(unique=True)
+
+    track_stock = models.BooleanField(default=True)
+    require_shipping = models.BooleanField(default=True)
+
+    options = models.ManyToManyField('Option')
+
+    def has_attribute(self):
+        return self.attributes.exists()
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.title
+
+
+class ProductAttribute(models.Model):
+
+    class AttributeTypeChoice(models.TextChoices):
+        text = 'text'
+        integer = 'integer'
+        float = 'float'
+        option = 'option'
+        multi_option = 'multi_option'
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True, related_name='attributes')
+    title = models.CharField(max_length=64)
+    type = models.CharField(max_length=16, choices=AttributeTypeChoice.choices, default=AttributeTypeChoice.text)
+    option_group = models.ForeignKey(OptionGroup, on_delete=models.PROTECT, null=True, blank=True)
+    required = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Product Attribute"
+        verbose_name_plural = "Product Attributes"
+
+    def __str__(self):
+        return self.title
+
+
+class Option(models.Model):
+    class OptionTypeChoice(models.TextChoices):
+        text = 'text'
+        integer = 'integer'
+        float = 'float'
+        option = 'option'
+        multi_option = 'multi_option'
+
+    title = models.CharField(max_length=64)
+    type = models.CharField(max_length=16, choices=OptionTypeChoice.choices, default=OptionTypeChoice.text)
+    option_group = models.ForeignKey(OptionGroup, on_delete=models.PROTECT, null=True, blank=True)
+    required = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Option"
+        verbose_name_plural = "Options"
+
+    def __str__(self):
+        return self.title
 
