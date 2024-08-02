d7e335dd39b001e8aebed1ebfb35e41df840939b
arminmahdavian <armin.mail007@gmail.com>
2024-08-02 09:32:32 +0100

    initiated Image model

--- catalogue/models.py
+++ catalogue/models.py
@@ -169,4 +169,20 @@ class ProductRecommendation(models.Model):
         ordering = ('primary', '-rank')
 
 
+class Image(models.Model):
+    title = models.CharField(max_length=128, null=True, blank=True)
+    image = models.ImageField(width_field='width', height_field='height', upload_to='images/')
+
+    width = models.IntegerField(editable=False)
+    height = models.IntegerField(editable=False)
+
+    file_hash = models.CharField(max_length=40, db_index=True)
+    file_size = models.PositiveIntegerField(null=True)
+
+    focal_point_x = models.PositiveIntegerField(null=True, blank=True)
+    focal_point_y = models.PositiveIntegerField(null=True, blank=True)
+    focal_point_width = models.PositiveIntegerField(null=True, blank=True)
+    focal_point_height = models.PositiveIntegerField(null=True, blank=True)
+
+
 
