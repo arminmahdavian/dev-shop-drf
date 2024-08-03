61d15e4d4fe464ce4e3810f928e0d908b2705507
arminmahdavian <armin.mail007@gmail.com>
2024-08-03 11:13:28 +0100

    updated User admin

--- auths/users/admin.py
+++ auths/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from auths.users.models import User
 
 # Register your models here.
+
+
+@admin.register(User)
+class DevShopUserAdmin(UserAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "verified_email")
+
